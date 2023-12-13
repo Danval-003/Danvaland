709b62b5a279b0c1ad960c6901a1fb6efdcbcf8e
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2023-12-13 14:24:53 -0600

    Empezamos el bingo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './App.css'
-import { StartPage, BingoPage } from '@pages'
+import { StartPage } from '@pages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<StartPage />} />
-      <Route path='/Bingo' element={<BingoPage />} />
     </Routes>
   </BrowserRouter>
 )

98dcaa4b2f22f817bcc7d1db87ee3c91770bade5
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2023-12-13 14:49:39 -0600

    Empezamos el bingo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { StartPage } from '@pages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
-  console.log(process.env.VITE_REACT_APP_SUPABASE_KEY)
+  console.log(process.env.SUPABASE_KEY)
   return (
     <BrowserRouter>
       <Routes>
